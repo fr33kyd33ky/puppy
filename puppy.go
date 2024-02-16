e6ad62dd16a2fc96e605d0cf642951a6805ef20d
Christian McMillan <christian.mcmillan@q2.com>
2024-02-16 12:00:52 -0600

    11

--- puppy.go
+++ puppy.go
@@ -1,6 +1,8 @@
 package puppy
 
 import (
+	"fmt"
+
 	"github.com/fr33kyd33ky/dog"
 )
 
@@ -18,4 +20,8 @@ func BigBark() string {
 
 func BigBarks() string {
 	return dog.WhenGrownUp(Barks())
+}
+
+func From11() {
+	fmt.Println("v11")
 }
