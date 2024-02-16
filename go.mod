29f11664acbaafd07ac55d3fbb86487140c14eb6
Christian McMillan <christian.mcmillan@q2.com>
2024-02-16 10:44:55 -0600

    added dog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fr33kyd33ky/puppy
 
 go 1.22.0
+
+require github.com/fr33kyd33ky/dog v0.0.0-20240216163109-c12b41e06767
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/fr33kyd33ky/dog"
+)
+
 func Bark() string {
 	return "Bark"
 }
 
 func Barks() string {
 	return "Bark Bark Bark"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
